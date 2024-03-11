7efc5e66f7b40e8152d30b5dd34aa1e4fde84028
Babatunde <babatundeojomu2016@gmail.com>
2024-03-11 05:01:17 +0100

    Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,45 @@
-:#!/usr/bin/python3
+#!/usr/bin/python3
 """
-Module for the BaseModel class.
+module for base_model
 """
-
 import uuid
+from models import storage
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        data = self.__dict__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        data['__class__'] = self.__class__.__name__
-        return data
+    """ implementing BaseModel class """
+    def __init__(self, *args, **kwargs):
+        """ intialization of BaseModel class """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ('created_at', 'updated_at'):
+                        formatted = datetime.fromisoformat(value)
+                        setattr(self, key, formatted)
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        """ overriding __str__ method """
+        return "[{}] ({}) {}".\
+            format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ updates updated_at with the current datetime """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ dictionary containing all keys/values of __dict__ """
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
